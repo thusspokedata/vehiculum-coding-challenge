d737be74f612aab510188a5ecbc9ba5ab87145d0
Antonio Saleme <79271990+thusspokedata@users.noreply.github.com>
2022-09-30 23:01:10 +0200

    create database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,14 @@
+import { join } from 'path';
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { VehiculumModule } from './vehiculum/vehiculum.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+    VehiculumModule,
+  ],
 })
 export class AppModule {}
